93934f4af6a9f3d7dfb341af4f4cb7e7763165a7
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-06-26 23:14:08 +1000

    DbConnectionWrongHost

---
+++ src/test/java/DataBaseConnections.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DataBaseConnections {
+    public static void mysql() throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/app";
+        String userName = "app";
+        String password = "9mREsvXDs9Gk89Ef";
+        Class.forName("com.mysql.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(url, userName, password)) {
+            System.out.println("We're connected");
+        }
+    }
+}
